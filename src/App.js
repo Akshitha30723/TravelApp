e84bded1d06d4d1d735d324fda6eaeee0217307f
Akshitha <44433079+Akshitha30723@users.noreply.github.com>
2021-09-17 12:54:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
-import Side from "./components/sidebar";
-import Favourites from "./components/Favourites";
-import Profile from "./components/Profile";
-import Notifications from "./components/Notifications";
-import Settings from "./components/Settings";
-import HelpMe from "@material-ui/icons/Help";
+
 class App extends Component{
   render(){
     return(
@@ -15,34 +10,17 @@ class App extends Component{
       <Router>
         <Switch>
           <Route exact path="/" component={Login}/>
-        </Switch>
-      </Router>
-      <div className="App">
-      <Router>
-        <Side/>
-        <Switch>
-        <Route exact path={"/dashboard"}> 
+          <Route exact path={"/dashboard"}> 
         <Dashboard/>
         </Route>
-        <Route exact path={"/fav"}> 
-        <Favourites/>
-        </Route>
-        <Route exact path={"/profile"}> 
-        <Profile/>
-        </Route>
-        <Route exact path={"/notifications"}> 
-        <Notifications/>
-        </Route>
-        <Route exact path={"/settings"}> 
-        <Settings/>
-        </Route>
-        <Route exact path={"/help"}> 
-        <HelpMe/>
-        </Route>
         </Switch>
       </Router>
+      
+       
+        
+       
       </div>
-      </div>
+    
     )
   }
 }

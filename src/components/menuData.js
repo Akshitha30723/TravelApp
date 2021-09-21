52a89ab39e8f52b3f3d467d41ecdb4bdf9ca8273
Akshitha <44433079+Akshitha30723@users.noreply.github.com>
2021-09-21 16:43:07 +0530

    Create menuData.js

---
+++ src/components/menuData.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import HomeIcon from '@material-ui/icons/Home';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+import ExploreIcon from '@material-ui/icons/Explore';
+import FlightIcon from '@material-ui/icons/Flight';
+export const menuData=[
+    {
+        name:'Accomodation',
+        icon:<HomeIcon/>
+    },
+    {
+        name:'Experience',
+        icon:<BeachAccessIcon/>
+    },
+    {
+        name:'Adventures',
+        icon:<ExploreIcon/>
+    },
+    {
+        name:'Flights',
+        icon:<FlightIcon/>
+    }
+
+]

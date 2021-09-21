653a8c73bac88f9e588967f4c09b6006d60c34af
Akshitha <44433079+Akshitha30723@users.noreply.github.com>
2021-09-21 16:41:18 +0530

    Create hotelData.js

---
+++ src/components/hotelData.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import image3 from '../assets/image3.jpg';
+import image7 from '../assets/image7.jpg';
+import image5 from '../assets/image5.jpg';
+export const hotelData=[
+{
+    img:image3,
+    name:"The Golden Hotel", 
+    desc:"Punjab", 
+    rating:"9.2"
+},
+{
+    img:image7, 
+    name:"Mountain Hotal", 
+    desc:"sinagar", 
+    rating:"9.1"
+},
+{
+    img:image5, 
+    name:"MG Hotel", 
+    desc:"Hyderabad", 
+    rating:"9"
+}
+
+]

36b2a89348ce1172f9c4e964149b60121154b8f1
Akshitha <44433079+Akshitha30723@users.noreply.github.com>
2021-09-21 16:39:47 +0530

    Create expData.js

---
+++ src/components/expData.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import image3 from '../assets/image3.jpg';
+import image7 from '../assets/image7.jpg';
+import image5 from '../assets/image5.jpg';
+import image6 from '../assets/image6.jpg';
+export const expData=[
+    {
+        img:image3,
+        desc:'The Golden Circle,',
+        place:' Iceland',
+        days:'5-7',
+        distance:'20km'
+    },
+    {
+        img:image7,
+        desc:'Snorkelling, ',
+        place:'Iceland',
+        days:'11',
+        distance:'20km'
+    },
+    {
+        img:image5, 
+        desc:'The Golden Circle, ',
+        place:'Iceland', 
+        days:'5-7',
+        distance:'20km'
+    },
+    {
+        img:image6,
+        desc:'Snorkelling, ',
+        place:'Iceland', 
+        days:'11',
+        distance:'20km'
+    }
+]

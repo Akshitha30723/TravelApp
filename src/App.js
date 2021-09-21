3533d438aa710504ded8b24dd7fa3ef6e423f1af
Akshitha <44433079+Akshitha30723@users.noreply.github.com>
2021-09-21 16:36:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
-
+import Favourites from "./components/Favourites";
+import Profile from "./components/Profile";
+import Notifications from './components/Notifications';
+import Settings from "./components/Settings"; 
+import Help from "./components/Help";
+import HotelList from "./components/HotelList";
 class App extends Component{
   render(){
     return(
@@ -13,12 +18,26 @@ class App extends Component{
           <Route exact path={"/dashboard"}> 
         <Dashboard/>
         </Route>
+        <Route exact path={"/fav"}> 
+        <Favourites/>
+        </Route>
+        <Route exact path={"/profile"}> 
+        <Profile/>
+        </Route>
+        <Route exact path={"/notifications"}> 
+        <Notifications/>
+        </Route>
+        <Route exact path={"/settings"}> 
+        <Settings/>
+        </Route>
+        <Route exact path={"/help"}> 
+        <Help/>
+        </Route>
+        <Route exact path={"/hotels"}> 
+        <HotelList/>
+        </Route>
         </Switch>
-      </Router>
-      
-       
-        
-       
+      </Router> 
       </div>
     
     )

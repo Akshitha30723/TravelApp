46f4ac20c239707c4a80b33915630f5000209b8b
Akshitha <44433079+Akshitha30723@users.noreply.github.com>
2021-09-21 16:43:57 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,28 +1,16 @@
 import React from 'react';
-import {ImHome2} from 'react-icons/im';
-import BeachAccessIcon from '@material-ui/icons/BeachAccess';
-import ExploreIcon from '@material-ui/icons/Explore';
-import FlightIcon from '@material-ui/icons/Flight';
+import MenuCard from './menu-card';
+import { menuData } from './menuData';
 function Menu(){
-    const style={width:45,height:25,color:'lightcoral'}
     return(
-<div>
-            <div className="menu1">
-            <button className="button"><ImHome2 style={style}/></button>
-            <div className="txt">Accomodation</div>
-            </div>
-            <div className="menu1">
-            <button className="button"><BeachAccessIcon style={style}/></button>
-            <div className="txt">Experiences</div>
-            </div>
-            <div className="menu1">
-            <button className="button"><ExploreIcon style={style}/></button>
-            <div className="txt">Adventures</div>
-            </div>
-            <div className="menu1">
-            <button className="button"><FlightIcon style={style}/></button>
-            <div className="txt">Flights</div>
-            </div>
+<div className="features">
+{menuData.map((e)=>{
+            return(
+                <MenuCard name={e.name} children={e.icon}/>
+            );
+        }
+        )}
+                         
             </div>
     )
 }
